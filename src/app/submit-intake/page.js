28fbcca5983432cd805e2bd1556de812f545a247
yoni <ymesa@louislawgroup.com>
2025-02-25 11:44:08 -0500

    temporary submit intake page

---
+++ src/app/submit-intake/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import Layout from "../components/Layout/Layout"; // Import Layout
+import ContactSection from "../components/Contact/ContactSection"; // Import Contact Component
+
+export default function FreeCaseEvaluation() {
+  return (
+    <Layout>
+      <ContactSection />
+    </Layout>
+  );
+}
