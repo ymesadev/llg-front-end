ecd420145089cc0b2e8a23d72bf699c3a1437d52
Rodnie Galvan <rsgalvan@affinitycorp.net>
2025-04-03 00:08:21 +0800

    fix mext button as well

--- public/icons/index.js
+++ public/icons/index.js
@@ -1,3 +1,4 @@
 export { default as ChatUsPopup } from "./ChatUsPopup";
 export { default as TextUsPopup } from "./TextUsPopup";
 export { default as ClosePopup } from "./ClosePopup";
+export { default as ArrowRight } from "./ArrowRight";
