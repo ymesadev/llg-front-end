584f79bc58460b5ad9cf0001d181e394738be819
ymesadev <ymesa@louislawgroup.com>
2026-02-28 10:33:39 -0500

    fix: add cases.js config for privacy-torts sign pages

---
+++ src/config/cases.js
@@ -0,0 +1,30 @@
+const caseConfig = {
+  "kin-insurance-privacy-torts": {
+    companyName: "Kin Insurance",
+    companyWebsite: "kin.com",
+    localStorageKey: "kin-contact",
+  },
+  "vuori-privacy-torts": {
+    companyName: "Vuori",
+    companyWebsite: "vuori.com",
+    localStorageKey: "vuori-contact",
+  },
+  "tower-hill-insurance-privacy-torts": {
+    companyName: "Tower Hill Insurance",
+    companyWebsite: "thig.com",
+    localStorageKey: "tower-hill-contact",
+  },
+  "slide-insurance-privacy-torts": {
+    companyName: "Slide Insurance",
+    companyWebsite: "slideinsurance.com",
+    localStorageKey: "slide-contact",
+  },
+  "american-integrity-insurance-privacy-torts": {
+    companyName: "American Integrity Insurance",
+    companyWebsite: "aii.com",
+    localStorageKey: "american-integrity-contact",
+  },
+};
+
+export const DOCUSEAL_TEMPLATE = "https://docuseal.com/d/DJJQhoJGYyp327";
+export default caseConfig;
